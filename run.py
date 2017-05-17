1504307d77aa467362410c66f76bc5586bb19379
Dirk von Grünigen <dirk@vongruenigen.com>
2017-05-17 22:55:32 +0200

    added a traditional autoencoder and finished implementing the training loop

--- run.py
+++ run.py
@@ -1,66 +1,108 @@
 #!/usr/bin/env python
 
 import os
+import re
 import sys
 import tensorflow as tf
 import h5py
+import time
 
 from os import path
+from numpy import random
 
 # Some mangling with the load path
 source_path = path.realpath('./source')
 sys.path.insert(0, source_path)
 
-from variational_autoencoder import VariationalAutoencoder
+from autoencoders import AutoEncoder, VariationalAutoencoder
 
 TRAINING_EPOCHS = 20
 BATCH_SIZE = 128
 DISPLAY_EPOCH = 1
-DISPLAY_BATCH = 100
+DISPLAY_BATCH = 1000
 HIDDEN_SIZE = 512
+RESULTS_DIR = path.abspath(path.join(path.dirname(__file__), 'results'))
+
+CurrentAutoEncoder = AutoEncoder
 
 mode = 'train'
 argv = sys.argv[1:]
 
-if len(argv) < 2:
-    print('ERROR: ./run.py <samples.py> <model-out>')
+if len(argv) < 1:
+    log('ERROR: ./run.py <samples.py>')
     sys.exit(2)
 
-def get_next_batch(train_data, epoch):
-    start_idx = epoch * BATCH_SIZE
-    end_idx = start_idx + BATCH_SIZE
+def camel_to_sneak(name):
+    '''Convert a string from camel-case to sneak-case.'''
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
-    return train_data[start_idx:end_idx]
+def log(msg, level='info'):
+    ts = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    print('[%s][%s] %s' % (level.upper(), ts, msg))
+
+encoder_type = camel_to_sneak(CurrentAutoEncoder.__name__)
+time_stamp = time.strftime('%Y-%m-%d_%H-%M-%S', time.localtime())
+result_name = '%s-%s-results' % (time_stamp, encoder_type)
+result_path = path.join(RESULTS_DIR, result_name)
+
+if not path.isdir(result_path):
+    os.mkdir(result_path)
+else:
+    log('ERROR: Result directory "%s" already exists' % result_path)
+    sys.exit(2)
+
+def get_next_batch(train_data, epoch, random=False, available_idxs=None):
+    # Gather random indices we want to use for this batch
+    idxs = None
+
+    if random:
+        idxs = [available_idxs.pop(random.randint(0, len(available_idxs))) \
+                for _ in range(BATCH_SIZE)]
+        idxs = list(sorted(idxs))
+    else:
+        start_idx = epoch*BATCH_SIZE
+        idxs = range(start_idx, start_idx+BATCH_SIZE)
+
+    batch_data = train_data[idxs]
+
+    if random:
+        return (batch_data, available_idxs)
+    else:
+        return batch_data
 
 with tf.Session() as session:
     if mode == 'train':
         train_data_path = argv[0]
-        model_out_path = argv[1]
 
         with h5py.File(train_data_path) as train_f:
             train_data = train_f['x']
             input_size = train_data.shape[1]
             num_samples = train_data.shape[0]
 
-            autoencoder = VariationalAutoencoder(input_size, HIDDEN_SIZE, session=session)
+            log('Starting training with a %s' % CurrentAutoEncoder.__name__)
+
+            autoencoder = CurrentAutoEncoder(input_size, HIDDEN_SIZE, session=session)
             saver = tf.train.Saver(tf.global_variables(), max_to_keep=3)
 
             session.run(tf.global_variables_initializer())
 
             for epoch in range(TRAINING_EPOCHS):
+                log('Starting epoch #%d' % (epoch+1))
                 avg_loss = 0
                 num_batches = int(num_samples / BATCH_SIZE)
 
                 for num_batch in range(num_batches):
                     batch_x = get_next_batch(train_data, epoch)
+
                     loss = autoencoder.batch_fit(batch_x)
                     avg_loss += (loss / num_samples) * BATCH_SIZE
 
-                    if (num_batch+1) % DISPLAY_BATCH == 0:
-                        print('Batch #%d of #%d, loss = %.5f' % (num_batch+1, num_batches, avg_loss))
+                    if (num_batch+1) % DISPLAY_BATCH == 0 or (num_batches - num_batch) < 5:
+                        log('Batch #%d of #%d, loss = %.5f' % (num_batch+1, num_batches, loss))
 
                 if (epoch+1) % DISPLAY_EPOCH == 0:
-                    print('Epoch #%d of #%d, loss = %.5f' % (epoch+1, TRAINING_EPOCHS, avg_loss))
-                    saver.save(session, model_out_path)
+                    log('Epoch #%d of #%d, loss = %.5f' % (epoch+1, TRAINING_EPOCHS, avg_loss))
+                    saver.save(session, result_path)
     else:
-        print('NOT IMPLEMENTED YET!')
+        log('GENERATING EMBEDDINGS NOT IMPLEMENTED YET!')
--- source/autoencoders.py
+++ source/autoencoders.py
@@ -1,5 +1,69 @@
 import tensorflow as tf
 
+class AutoEncoder(object):
+    def __init__(self, input_size, hidden_size, session):
+        '''Initializes a new instance of the VariationalAutoencoder class.'''
+        self.session = session
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.weights = {}
+        self.transfer_fn = tf.nn.softplus
+
+        self.__initialize()
+        self.__build()
+
+    def __initialize(self):
+        '''Initializes the weights needed to build the computational graph.'''
+        weights = {}
+
+        weights['weights_1'] = tf.get_variable('weights_1',[self.input_size, self.hidden_size])
+        weights['bias_1'] = tf.Variable(tf.zeros([self.hidden_size], dtype=tf.float32))
+        weights['weights_2'] = tf.Variable(tf.zeros([self.hidden_size, self.input_size], dtype=tf.float32))
+        weights['bias_2'] = tf.Variable(tf.zeros([self.input_size], dtype=tf.float32))
+
+        self.weights = weights
+
+    def get_optimizer(self):
+        '''Returns the optimizer for this instance.'''
+        return self.optimizer
+
+    def get_loss(self):
+        '''Returns the loss function for this instance.'''
+        return self.loss_fn
+
+    def get_weights_and_biases(self):
+        '''Returns the weights and biases of this instance.'''
+        return self.weights
+
+    def get_internal_representation(self):
+        '''Returns the internal, embedded representation variables.'''
+        return self.hidden
+
+    def batch_fit(self, input):
+        '''Fits the model to the given batch.'''
+        loss, _ = self.session.run((self.loss_fn, self.optimizer),
+                                   feed_dict={self.input: input})
+        return loss
+
+    def transform(self, input):
+        return self.session.run(self.hidden, feed_dict={self.input: input})
+
+    def __build(self):
+        '''Builds the computational graph.'''
+        self.input = tf.placeholder(tf.float32, [None, self.input_size])
+
+        hidden_1_result = tf.matmul(self.input, self.weights['weights_1'])
+        self.hidden = self.transfer_fn(tf.add(hidden_1_result,
+                                              self.weights['bias_1']))
+
+        reconstruction_result = tf.matmul(self.hidden, self.weights['weights_2'])
+        self.reconstruction = tf.add(reconstruction_result, self.weights['bias_2'])
+
+        diff = tf.subtract(self.reconstruction, self.input)
+        self.loss_fn = 0.5 * tf.reduce_sum(tf.pow(diff, 2.0))
+        self.optimizer_fn = tf.train.AdamOptimizer(learning_rate=0.001)
+        self.optimizer = self.optimizer_fn.minimize(self.loss_fn)
+
 class VariationalAutoencoder(object):
     def __init__(self, input_size, hidden_size, session):
         '''Initializes a new instance of the VariationalAutoencoder class.'''
@@ -50,8 +114,8 @@ class VariationalAutoencoder(object):
         return self.session.run(self.z_mean, feed_dict={self.input: input})
 
     def partial_fit(self, X):
-        cost, opt = self.sess.run((self.cost, self.optimizer), feed_dict={self.x: X})
-        return cost
+        loss, opt = self.sess.run((self.loss_fn, self.optimizer), feed_dict={self.x: X})
+        return loss
 
     def transform(self, X):
         return self.sess.run(self.z_mean, feed_dict={self.x: X})
