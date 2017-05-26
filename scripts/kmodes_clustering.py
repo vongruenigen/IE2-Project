e66d89a3e65f6d5730c12e0ea96953eb89c3d79d
Dirk von Grünigen <dirk@vongruenigen.com>
2017-05-26 23:46:09 +0200

    fixed some problems related to the kmodes script

--- scripts/kmodes_clustering.py
+++ scripts/kmodes_clustering.py
@@ -2,21 +2,13 @@ from pyspark import SparkConf, SparkContext
 from pyspark_kmodes import EnsembleKModes
 from pyspark.sql import SparkSession
 from numpy import array
+from collections import defaultdict
 
 import multiprocessing
 import json
 import math
 
 APP_NAME = 'IE2-Project-Homocide-Reports'
-
-
-"""
-An example demonstrating k-means clustering.
-Run with:
-  bin/spark-submit examples/src/main/python/ml/kmeans_example.py
-
-This example requires NumPy (http://www.numpy.org/).
-"""
 
 def run_kmodes(sc):
     cpu_count = multiprocessing.cpu_count()
@@ -27,15 +19,15 @@ def run_kmodes(sc):
         dataset = sc.textFile("/media/dvg/Volume/Dropbox/ZHAW/IE2/Project/data/samples.csv", cpu_count)
         dataset = dataset.map(lambda line: array([float(x) for x in line.split(';')]))
 
-        method = EnsembleKModes(n, 50)
+        method = EnsembleKModes(n, 20)
         model = method.fit(dataset)
 
-        cluster_infos[n]['cost'] = model.mean_cost
-        cluster_infos[n]['centroids'] = model.clusters
+        cluster_infos['cost'] = model.mean_cost
+        cluster_infos['centroids'] = model.clusters
 
-    # Write Cluster loss pairs into json file
-    with open('/media/dvg/Volume/Dropbox/ZHAW/IE2/Project/results/kmodes_results.json', 'w') as out_f:
-        json.dump(cluster_infos, out_f)
+        # Write Cluster loss pairs into json file
+        with open('/media/dvg/Volume/Dropbox/ZHAW/IE2/Project/results/kmodes_%d_results.json' % n, 'w') as out_f:
+            json.dump(cluster_infos, out_f)
 
 if __name__ == "__main__":
     conf = SparkConf().setAppName(APP_NAME)
