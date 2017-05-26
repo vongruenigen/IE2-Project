5de64c06f6e70682b5ac90458580321ba3a423a6
Dirk von Grünigen <dirk@vongruenigen.com>
2017-05-26 23:21:51 +0200

    updated both clustering scripts to use all cpus, added script for kmodes

--- scripts/kmodes_clustering.py
+++ scripts/kmodes_clustering.py
@@ -1,10 +1,6 @@
-from __future__ import print_function
-
 from pyspark import SparkConf, SparkContext
-
-from pyspark.mllib.clustering import KMeans
+from pyspark_modes import EnsembleKModes
 from pyspark.sql import SparkSession
-from pyspark.ml.feature import VectorAssembler
 from numpy import array
 
 import multiprocessing
@@ -22,9 +18,9 @@ Run with:
 This example requires NumPy (http://www.numpy.org/).
 """
 
-def run_kmeans(sc):
+def run_kmodes(sc):
     cpu_count = multiprocessing.cpu_count()
-    cluster_loss = dict()
+    cluster_infos = {}
 
     # Loads data.
     for n in range(2, 10):
@@ -32,8 +28,8 @@ def run_kmeans(sc):
         dataset = sc.parallelize(dataset, cpu_count)
         dataset = dataset.map(lambda line: array([float(x) for x in line.split(';')]))
 
-        # Trains a k-means model.all
-        clusters = KMeans.train(dataset, n)
+        method = EnsembleKModes(n, 50)
+        model = method.fit(dataset)
 
         def error(point):
             center = clusters.centers[clusters.predict(point)]
@@ -44,15 +40,16 @@ def run_kmeans(sc):
         print("Within Set Sum of Squared Errors = " + str(wssse))
 
         clusters.save(sc, "/media/dvg/Volume/Dropbox/ZHAW/IE2/Project/results/kmeans_model_%d" % n)
-        cluster_loss[n] = wssse
+
+        cluster_infos[n]['cost'] = model.mean_cost
+        cluster_infos[n]['centroids'] = model.clusters
 
     # Write Cluster loss pairs into json file
     with open('/media/dvg/Volume/Dropbox/ZHAW/IE2/Project/results/kmeans_elbowCurveData.json', 'w') as out_f:
-        json.dump(cluster_loss, out_f)
-
+        json.dump(cluster_infos, out_f)
 
 if __name__ == "__main__":
     conf = SparkConf().setAppName(APP_NAME)
     conf.setMaster('local[*]')
     sc = SparkContext(conf=conf)
-    run_kmeans(sc)
+    run_kmodes(sc)
