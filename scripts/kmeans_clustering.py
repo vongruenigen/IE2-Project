2541aabd18d4c9e5e8bba6fa82ec1ec50ed44340
weilemar <weilemar@students.zhaw.ch>
2017-05-27 21:47:11 +0200

    update jupyter notebook and scripts

--- scripts/kmeans_clustering.py
+++ scripts/kmeans_clustering.py
@@ -1,10 +1,6 @@
-from __future__ import print_function
 
 from pyspark import SparkConf, SparkContext
-
 from pyspark.mllib.clustering import KMeans
-from pyspark.sql import SparkSession
-from pyspark.ml.feature import VectorAssembler
 from numpy import array
 
 import multiprocessing
@@ -13,22 +9,13 @@ import math
 
 APP_NAME = 'IE2-Project-Homocide-Reports'
 
-
-"""
-An example demonstrating k-means clustering.
-Run with:
-  bin/spark-submit examples/src/main/python/ml/kmeans_example.py
-
-This example requires NumPy (http://www.numpy.org/).
-"""
-
 def run_kmeans(sc):
     cpu_count = multiprocessing.cpu_count()
     cluster_loss = dict()
 
     # Loads data.
     for n in range(2, 10):
-        dataset = sc.textFile("/media/dvg/Volume/Dropbox/ZHAW/IE2/Project/data/samples.csv", cpu_count)
+        dataset = sc.textFile(INPUT_DATA, cpu_count)
         dataset = dataset.map(lambda line: array([float(x) for x in line.split(';')]))
 
         # Trains a k-means model.all
@@ -42,11 +29,11 @@ def run_kmeans(sc):
         wssse = dataset.map(lambda x: error(x)).reduce(lambda x, y: x+y)
         print("Within Set Sum of Squared Errors = " + str(wssse))
 
-        clusters.save(sc, "/media/dvg/Volume/Dropbox/ZHAW/IE2/Project/results/kmeans_model_%d" % n)
+        clusters.save(sc, OUTPUT_MODEL_PATH + "_%d" % n)
         cluster_loss[n] = wssse
 
     # Write Cluster loss pairs into json file
-    with open('/media/dvg/Volume/Dropbox/ZHAW/IE2/Project/results/kmeans_elbowCurveData.json', 'w') as out_f:
+    with open( OUTPUT_KMEANS_CURVE, 'w') as out_f:
         json.dump(cluster_loss, out_f)
 
 
