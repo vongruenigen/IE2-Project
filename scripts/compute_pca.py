eb8ddadd2051c364c46d2c68f8a97e331108f9d8
Dirk von Grünigen <dirk@vongruenigen.com>
2017-05-27 22:31:21 +0200

    some modifications in the pca script

--- scripts/compute_pca.py
+++ scripts/compute_pca.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 from pyspark import SparkConf, SparkContext
 
 from pyspark.mllib.feature import PCA
+from pyspark.mllib.linalg import Vectors
 from pyspark.sql import SparkSession
 from numpy import array
 
@@ -17,15 +18,15 @@ def run_pca(sc):
     cluster_loss = dict()
 
     dataset = sc.textFile("/media/dvg/Volume/Dropbox/ZHAW/IE2/Project/data/emb-out.csv", cpu_count)
-    dataset = dataset.map(lambda line: array([float(x) for x in line.split(';')]))
+    dataset = dataset.map(lambda line: Vectors.dense([float(x) for x in line.split(';')]))
 
     model = PCA(2).fit(dataset)
     transformed = model.transform(dataset)
     transformed_csv = transformed.map(lambda x: ';'.join(list(map(str, x))))
-    transformed_csv.saveAsTextFile('/media/dvg/Volume/Dropbox/ZHAW/IE2/Project/data/emb-pca-out.csv')
+    transformed_csv.coalesce(1).saveAsTextFile('/media/dvg/Volume/Dropbox/ZHAW/IE2/Project/data/emb-pca-out')
 
 if __name__ == "__main__":
     conf = SparkConf().setAppName(APP_NAME)
     conf.setMaster('local[*]')
     sc = SparkContext(conf=conf)
-    run_kmeans(sc)
+    run_pca(sc)
