2384dc5f6470c45cd24972430608af00204614e7
weilemar <weilemar@students.zhaw.ch>
2017-05-27 21:52:16 +0200

    add variables

--- scripts/predict_cluster.py
+++ scripts/predict_cluster.py
@@ -8,6 +8,9 @@ import os
 
 APP_NAME = 'IE2-Project-Homocide-Reports'
 INPUT_DATA = "C:/Users/MWeil/Documents/GitHub/IE2-Project/data/homicide-reports/database_new.csv"
+OUTPUT_LABEL = "C:/Users/MWeil/Documents/GitHub/IE2-Project/data/homicide-reports/database_new_label.json"
+INPUT_MODEL = "C:/Users/MWeil/Documents/GitHub/IE2-Project/one-hot-clustering/kmeans_model_7"
+
 def run_kmeans(sc):
     cpu_count = multiprocessing.cpu_count()
 
@@ -16,13 +19,13 @@ def run_kmeans(sc):
     dataset = dataset.map(lambda line: array([float(x) for x in line.split(';')]))
 
     # Load Model
-    sameModel = KMeansModel.load(sc, "C:/Users/MWeil/Documents/GitHub/IE2-Project/one-hot-clustering/kmeans_model_7")
+    sameModel = KMeansModel.load(sc, INPUT_MODEL)
 
     # Predict cluster labels per row
     labels = sameModel.predict(dataset).collect()
 
     # Save labels in json file
-    with open("C:/Users/MWeil/Documents/GitHub/IE2-Project/data/homicide-reports/database_new_label.json", 'w') as out_f:
+    with open(OUTPUT_LABEL, 'w') as out_f:
         json.dump(labels, out_f)
 
 if __name__ == "__main__":
